8c0e88be8df9dcc89dcf0cfadd3c3aa7841c7fb7
AnushahNadir <34958453+AnushahNadir@users.noreply.github.com>
2022-03-26 13:54:44 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// console.log("fetch API")
+// const api = fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(function(res){
+//         console.log(res, "res")
+//     })
+// console.log(api, "API")
+
