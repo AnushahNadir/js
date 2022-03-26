3694a715a3e235e31b8118a8cc31555fa38791f3
AnushahNadir <34958453+AnushahNadir@users.noreply.github.com>
2022-03-26 14:36:26 +0500

    postAPI
    Work on postAPI

--- app.js
+++ app.js
@@ -5,13 +5,33 @@
 //     })
 // console.log(api, "API")
 
-async function fetchAPI(){
-    const api = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+// async function getAPI(){
+//     const api = await fetch('https://jsonplaceholder.typicode.com/todos/1')
     
+//     console.log(api, "API")
+//     const json = await api.json()
+//     console.log(json, "json")
+    
+// }
+//default method is get
+
+//getAPI();
+
+async function postapi(){
+    const api = await fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'bootcamp',
+            body: 'bar',
+            userId: 1
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    })
+
     console.log(api, "API")
     const json = await api.json()
     console.log(json, "json")
-    
 }
-
-fetchAPI();
+postapi();
